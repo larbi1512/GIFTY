6c7a6fe4415b3457507193dd9952b239c1be1e12
larbi <99117566+larbi1512@users.noreply.github.com>
2023-12-02 18:09:16 +0100

    wishlist added

---
+++ gifty/lib/presentation/wishlist/favorites.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:gifty/config/colors.config.dart';
+import 'package:gifty/widgets/favorite_item.dart';
+
+class wishList extends StatefulWidget {
+  const wishList({Key? key}) : super(key: key);
+
+  @override
+  State<wishList> createState() => _wishlistState();
+}
+
+class _wishlistState extends State<wishList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(255, 242, 238, 1.0),
+      body: Column(
+        children: [
+        
+           Expanded(
+            child: RawScrollbar(
+              child: SingleChildScrollView(
+                child:
+                    buildGridView(), 
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildGridView() {
+    return GridView.builder(
+      shrinkWrap: true,
+      padding: EdgeInsets.all(10),
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 6,
+        childAspectRatio: 0.7,
+
+        
+      ),
+      itemCount: 8, // Change this to the number of items you want to display
+      itemBuilder: (BuildContext context, int index) {
+        return LikedItemWidget();
+      },
+    );
+  }
+}
