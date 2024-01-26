23f2316225c367477635686944471490247d1df7
larbi <99117566+larbi1512@users.noreply.github.com>
2024-01-26 18:14:17 +0100

    changesé

--- gifty/lib/presentation/auth/sign_up/signup_provider.dart
+++ gifty/lib/presentation/auth/sign_up/signup_provider.dart
@@ -9,6 +9,10 @@ import '../../../widgets/fields/TagsInput.dart';
 import '../../../widgets/background_image.dart';
 import '../../../widgets/fields/categoryDropdown.dart';
 import '../../../widgets/rounded_container.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../../../constants/endpoints.dart';
+
 
 class signupProvider extends StatefulWidget {
   @override
@@ -17,6 +21,73 @@ class signupProvider extends StatefulWidget {
 
 class _SignupProviderState extends State<signupProvider> {
   String selectedCategory = 'Categories';
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController wilayaController = TextEditingController();
+  final TextEditingController phoneNumberController = TextEditingController();
+  final TextEditingController tagsController = TextEditingController();
+
+  bool isLoading = false;
+  
+  
+  Future<void> signupProviderPart1(String providerId) async {
+    setState(() {
+      isLoading = true;
+    });
+
+    final String username = usernameController.text;
+    final String wilaya = wilayaController.text;
+    final String phoneNumber = phoneNumberController.text;
+    final String selectedCategory = this.selectedCategory;
+    final String tags = tagsController.text;
+
+     final Map<String, String> data = {
+      'provider_id': providerId,
+      'username': username,
+      'wilaya': wilaya,
+      'phone_number': phoneNumber,
+      'category': selectedCategory,
+      'tags': tags,
+    };
+    try {
+      final response = await http.post(
+       Uri.parse(api_endpoint_provider_signup_1,),
+        body: data,
+      );
+
+      print('Response status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+    
+     if (response.statusCode == 200) {
+        // Successful signup part 1
+        final Map<String, dynamic> responseData = json.decode(response.body);
+        print(responseData);
+        // Navigate to the next screen or perform any other actions
+        Navigator.pushReplacementNamed(context, '/finish_signup_provider');
+      } else {
+        // Handle errors
+        final Map<String, dynamic> responseData = json.decode(response.body);
+        final String errorMessage = responseData['message'];
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(errorMessage),
+            backgroundColor: Colors.red,
+          ),
+        );
+      }
+    } catch (e) {
+      print('Error during signup part 1: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('An error occurred during signup part 1'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +100,6 @@ class _SignupProviderState extends State<signupProvider> {
       'Bakery',
       'Clothings',
       'Handicrafts',
-      // Add more categories as needed
     ];
 return Scaffold(
       body: BackgroundImage(
@@ -65,11 +135,11 @@ return Scaffold(
                       ),
                     ),
                     SizedBox(height: 30),
-                    //WilayaField(),
+                    WilayaField(controller: wilayaController,),
                     SizedBox(height: 10),
-                    //PhoneNumberField(),
+                    PhoneNumberField(controller: phoneNumberController,),
                     SizedBox(height: 10,),
-                    //UsernameField(hintText: "Store name"),
+                    UsernameField(hintText: "Store name", controller: usernameController,),
                     SizedBox(height: 10,),
                     CategoryDropdown(
                       categories: categories,
@@ -92,7 +162,9 @@ return Scaffold(
                         ),
                       ),
                       onPressed: () {
-                        Navigator.pushReplacementNamed(context, '/finish_signup_provider');
+                        isLoading
+                            ? null
+                            : signupProviderPart1(ModalRoute.of(context)!.settings.arguments.toString());
                       },
                       child: Text('Sign up'),
                     ),
