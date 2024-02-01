a8d10492f0eeb24d04781ff4c5b1b8e941980cd0
larbi <99117566+larbi1512@users.noreply.github.com>
2024-02-01 19:03:23 +0100

    Update endpoints.dart

--- gifty/lib/constants/endpoints.dart
+++ gifty/lib/constants/endpoints.dart
@@ -1,39 +1,10 @@
-// const api_endpoint_user_login = 'https://gifty-back.vercel.app/api/user_login';
-// const api_endpoint_user_signup =
-//     'https://gifty-back.vercel.app/api/signup_user';
-// const api_endpoint_provider_signup =
-//     'https://gifty-back.vercel.app/api/signup_provider';
-// const api_endpoint_user_signup_1 =
-//     'https://gifty-back.vercel.app/api/signup_user1';
-// const api_endpoint_provider_signup_1 =
-//     'https://gifty-back.vercel.app/api/signup_provider1';
-// const api_endpoint_finish_signup_user =
-//     'https://gifty-back.vercel.app/api/finish_signup_user';
-// const api_endpoint_finish_signup_provider =
-//     'https://gifty-back.vercel.app/api/finish_signup_provider';
-// const api_endpoint_get_user_data =
-//     'https://gifty-back.vercel.app/api/get_user_data';
-// const api_endpoint_get_provider_data =
-//     'https://gifty-back.vercel.app/api/get_provider_data';
-
-// const api_endpoint = 'https://gifty-back.vercel.app';
-
-const api_endpoint_user_login = 'http://192.168.8.101:5000/api/user_login';
-const api_endpoint_user_signup =
-    'http://192.168.8.101:5000/api/signup_user';
-const api_endpoint_provider_signup =
-    'http://192.168.8.101:5000/api/signup_provider';
-const api_endpoint_user_signup_1 =
-    'http://192.168.8.101:5000/api/signup_user1';
-const api_endpoint_provider_signup_1 =
-    'http://192.168.8.101:5000/api/signup_provider1';
-const api_endpoint_finish_signup_user =
-    'http://192.168.8.101:5000/api/finish_signup_user';
-const api_endpoint_finish_signup_provider =
-    'http://192.168.8.101:5000/api/finish_signup_provider';
-const api_endpoint_get_user_data =
-    'http://192.168.8.101:5000/api/get_user_data';
-const api_endpoint_get_provider_data =
-    'http://192.168.8.101:5000/api/get_provider_data';
-
-const api_endpoint = 'http://192.168.8.101:5000';
+const api_endpoint_user_login = 'https://gifty-back.vercel.app/api/user_login';
+const api_endpoint_user_signup ='https://gifty-back.vercel.app/api/signup_user';
+const api_endpoint_provider_signup =   'https://gifty-back.vercel.app/api/signup_provider';
+const api_endpoint_user_signup_1 = 'https://gifty-back.vercel.app/api/signup_user1';
+const api_endpoint_provider_signup_1 ='https://gifty-back.vercel.app/api/signup_provider1';
+const api_endpoint_finish_signup_user ='https://gifty-back.vercel.app/api/finish_signup_user';
+const api_endpoint_finish_signup_provider = 'https://gifty-back.vercel.app/api/finish_signup_provider';
+const api_endpoint_get_user_data ='https://gifty-back.vercel.app/api/get_user_data';
+ const api_endpoint_get_provider_data ='https://gifty-back.vercel.app/api/get_provider_data';
+ const api_endpoint = 'https://gifty-back.vercel.app';
